1c66be8d2ce00bd2d76a042880348c6059f78120
Joice Jessica <joicejssc@gmail.com>
2017-05-13 19:34:29 +0700

    Add New York Time API

--- app/src/main/java/id/sch/smktelkom_mlg/privateassignment/xirpl615/artikelku/HomeFragment.java
+++ app/src/main/java/id/sch/smktelkom_mlg/privateassignment/xirpl615/artikelku/HomeFragment.java
@@ -1,18 +1,39 @@
 package id.sch.smktelkom_mlg.privateassignment.xirpl615.artikelku;
 
 
+import android.app.ProgressDialog;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class HomeFragment extends Fragment {
-
+    private static final String URL_DATA = "https://api.nytimes.com/svc/movies/v2/reviews/search.json?api-key=74ce2c37a9f84c3da2f735d8dc538164";
+    private List<ArtikelList> artikelLists;
+    private RecyclerView recyclerView;
+    private ArtikelAdapter adapter;
 
     public HomeFragment() {
         // Required empty public constructor
@@ -23,7 +44,65 @@ public class HomeFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        View view = inflater.inflate(R.layout.fragment_home, container, false);
+        recyclerView = (RecyclerView) view.findViewById(R.id.recyclerView);
+        LinearLayoutManager layoutManager = new LinearLayoutManager(getActivity());
+        recyclerView.setLayoutManager(layoutManager);
+        artikelLists = new ArrayList<>();
+
+        isidata();
+        return view;
+    }
+
+    private void isidata() {
+        final ProgressDialog progressDialog = new ProgressDialog(getActivity());
+        progressDialog.setMessage("Loading data...");
+        progressDialog.show();
+
+
+        StringRequest stringRequest = new StringRequest(Request.Method.GET,
+                URL_DATA,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String s) {
+                        progressDialog.dismiss();
+                        try {
+                            JSONObject jsonObject = new JSONObject(s);
+                            JSONArray array = jsonObject.getJSONArray("results");
+
+                            //JSONArray array = jsonObject.getJSONObject("data").getJSONArray("results");
+                            //JSONArray array2 = jsonObject.getJSONArray("multimedia");
+
+                            for (int i = 0; i < array.length(); i++) {
+                                JSONObject o = array.getJSONObject(i);
+                                ArtikelList item = new ArtikelList(
+                                        o.getJSONObject("multimedia").getString("src"),
+                                        o.getString("display_title"),
+                                        o.getString("byline")
+                                );
+                                artikelLists.add(item);
+                            }
+                            adapter = new ArtikelAdapter(artikelLists, getActivity().getApplicationContext());
+                            recyclerView.setAdapter(adapter);
+
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError volleyError) {
+                        progressDialog.dismiss();
+                        Toast.makeText(getActivity().getApplicationContext(), volleyError.getMessage(), Toast.LENGTH_LONG).show();
+                    }
+                });
+
+        RequestQueue requestQueue = Volley.newRequestQueue(getActivity());
+        requestQueue.add(stringRequest);
+
     }
 
 }
+
